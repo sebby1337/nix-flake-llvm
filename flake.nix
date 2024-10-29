708aa9594545d2431a5cf6d99c45147b2dda9bc7
therlanda <gibson@gibsonsec.net>
2024-10-29 22:09:12 +0000

    optimisation profiles for flexible pipeline configurations

--- flake.nix
+++ flake.nix
@@ -1,56 +1,93 @@
-{
-  description = "Automated LLVM IR optimisation pipelines using Nix flakes";
+{ pkgs ? import <nixpkgs> {} }:
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+let
+  llvm = pkgs.llvmPackages_16.llvm;
+
+  mkOptStage = { name, input, passes, output, enableCustomPass ? true }:
+    pkgs.writeShellScriptBin "stage-${name}" ''
+      if [ -f ${output} ]; then
+        echo "Using cached ${output}"
+      else
+        ${llvm}/bin/opt \
+          ${if enableCustomPass then "-load-pass-plugin=$CUSTOM_PASSES_PATH" else ""} \
+          ${toString (lib.concatStringsSep " " passes)} \
+          -S ${input} \
+          -o ${output}
+      fi
+    '';
+
+  profiles = {
+    light = {
+      stage1 = mkOptStage {
+        name = "light-initial";
+        input = "./main.ll";
+        passes = ["-passes=mem2reg"];
+        output = "./light-stage1.ll";
+      };
+      stage2 = mkOptStage {
+        name = "light-final";
+        input = "./light-stage1.ll";
+        passes = ["-passes=dce"];
+        output = "./optimized.ll";
+      };
+    };
+
+    aggressive = {
+      stage1 = mkOptStage {
+        name = "aggressive-initial";
+        input = "./main.ll";
+        passes = [
+          "-passes=mem2reg"
+          "-passes=instcombine"
+          "-passes=simplifycfg"
+        ];
+        output = "./aggressive-stage1.ll";
+      };
+      stage2 = mkOptStage {
+        name = "aggressive-final";
+        input = "./aggressive-stage1.ll";
+        passes = [
+          "-passes=gvn"
+          "-passes=licm"
+          "-passes=loop-unroll"
+          "-passes=inline"
+        ];
+        output = "./optimized.ll";
+      };
+    };
+
+    custom = {
+      stage1 = mkOptStage {
+        name = "custom-initial";
+        input = "./main.ll";
+        passes = [
+          "-passes=mem2reg"
+          "-passes=custom-pass"
+        ];
+        output = "./custom-stage1.ll";
+      };
+      stage2 = mkOptStage {
+        name = "custom-final";
+        input = "./custom-stage1.ll";
+        passes = ["-passes=dce"];
+        output = "./optimized.ll";
+      };
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            llvmPackages_16.llvm
-            llvmPackages_16.clang
-            llvmPackages_16.lld
-            llvmPackages_16.lldb
+  pipeline = profile: pkgs.writeShellScriptBin "run-pipeline-${profile}" ''
+    export CUSTOM_PASSES_PATH="$(pwd)/src/pass/build/libCustomPasses.so"
+    
+    for stage in ${toString (lib.concatStringsSep " " (builtins.attrNames profiles.${profile}))}; do
+      ${profiles.${profile}.${stage}}/bin/${"stage-${profile}-${stage}"}
+    done
+    
+    echo "Pipeline complete. Optimised IR written to optimized.ll"
+  '';
 
-            cmake
-            gnumake
-            ninja
-
-            gdb
-            valgrind
-          ];
-
-          shellHook = ''
-            echo "Automated LLVM IR optimisation pipelines using Nix flakes"
-            echo "LLVM Version: ${pkgs.llvmPackages_16.llvm.version}"
-          '';
-        };
-
-        packages.default = pkgs.stdenv.mkDerivation {
-          name = "llvm-ir-optimizer";
-          src = ./.;
-          
-          buildInputs = with pkgs; [
-            llvmPackages_16.llvm
-            llvmPackages_16.clang
-          ];
-
-          buildPhase = ''
-            # Build commands will be specified in run.sh
-            echo "Building using run.sh..."
-          '';
-
-          installPhase = ''
-            mkdir -p $out/bin
-            # Copy necessary artifacts to output
-          '';
-        };
-      });
+in {
+  inherit profiles;
+  runLightPipeline = pipeline "light";
+  runAggressivePipeline = pipeline "aggressive";
+  runCustomPipeline = pipeline "custom";
 }
